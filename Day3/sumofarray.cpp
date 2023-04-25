fcea6c4b870b0cafaca151da80a43fcb3e64a53a
Divyank Kumar Chaudhary <96334672+divyank30@users.noreply.github.com>
2023-04-25 20:27:56 +0530

    Create sumofarray.cpp

---
+++ Day3/sumofarray.cpp
@@ -0,0 +1,36 @@
+Sum of array
+
+//{ Driver Code Starts
+#include <iostream>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+    public:
+    int getSum(int a[], int n) {
+        // Your code goes here
+        int i=0,sum=0;
+        for(i=0;i<n;i++)
+        {
+            sum=sum+a[i];
+        }
+        return sum;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int a[n];
+        for (int i = 0; i < n; i++) cin >> a[i];
+        Solution ob;
+        cout << ob.getSum(a, n) << endl;
+    }
+
+    return 0;
+}
+// } Driver Code Ends
