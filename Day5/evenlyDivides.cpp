c724aba409a76fc71efdd09a5d76f3e858e7005d
Divyank Kumar Chaudhary <96334672+divyank30@users.noreply.github.com>
2023-04-27 19:16:18 +0530

    Create evenlyDivides.cpp

---
+++ Day5/evenlyDivides.cpp
@@ -0,0 +1,42 @@
+https://practice.geeksforgeeks.org/problems/count-digits5716/1?page=1&difficulty[]=-2&status[]=solved&sortBy=submissions
+
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+public:
+    int evenlyDivides(int N){
+        //code here
+        int temp,count=0;
+        temp=N;
+        while(temp>0)
+        {
+         int rem=temp%10;
+         if(rem!=0 && N%rem==0)
+         {
+             count++;
+         }
+            
+           temp=temp/10;
+        }
+        return count;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        cout << ob.evenlyDivides(N) << endl;
+    }
+    return 0; 
+}
+// } Driver Code Ends
