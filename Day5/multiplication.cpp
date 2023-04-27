24aa1ae2d3acc04499f599f79cf15cb4f53790c0
Divyank Kumar Chaudhary <96334672+divyank30@users.noreply.github.com>
2023-04-27 19:13:31 +0530

    Create multiplication.cpp

---
+++ Day5/multiplication.cpp
@@ -0,0 +1,33 @@
+https://practice.geeksforgeeks.org/problems/c-input-output2432/1?page=2&difficulty[]=-2&sortBy=submissions
+
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+class Solution{   
+public:
+    int multiplication(int A, int B){
+        // code here 
+        int c;
+        c=A*B;
+        return c;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int A, B;
+        cin >> A >> B;
+        Solution ob;
+        cout << ob.multiplication(A,B) << endl;
+    }
+    return 0; 
+} 
+
+// } Driver Code Ends
